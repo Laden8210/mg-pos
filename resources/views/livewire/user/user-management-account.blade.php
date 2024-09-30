f7c33605a4aab50880e315780f31e0050d626ea4
Laden8210 <domingo.laden@gmail.com>
2024-09-30 14:40:32 +0800

    update

--- resources/views/livewire/user/user-management-account.blade.php
+++ resources/views/livewire/user/user-management-account.blade.php
@@ -39,7 +39,7 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                @if ($employees)
+                                @if ($employees != null)
 
                                     @foreach ($employees as $e)
                                         <tr>
